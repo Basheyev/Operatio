b2ac6217acb255351a5548a86eb572b14fbc0b29
Bolat Basheyev <b.basheyev@gmail.com>
2020-08-16 16:14:54 +0600

    New operation implementation

---
+++ app/src/main/java/com/axiom/operatio/production/machines/Operation.java
@@ -0,0 +1,42 @@
+package com.axiom.operatio.production.machines;
+
+import com.axiom.operatio.production.materials.Material;
+
+public class Operation {
+
+    protected int operationTime;                // Время операции в циклах производства
+    protected Material[] outputMaterials;       // Список кодов входящих материалов
+    protected Material[] inputMaterials;        // Список кодов исходящих материалов
+    protected int[] outputAmount;               // Список количества входящих материалов
+    protected int[] inputAmount;                // Список количества исходящих материалов
+
+
+
+    public boolean isCorrectInput(Material m) {
+        for (Material material: inputMaterials) {
+            if (material.equals(m)) return true;
+        }
+        return false;
+    }
+
+
+
+
+    public int totalInputAmount() {
+        int total = 0;
+        for (int i=0; i < inputAmount.length; i++) {
+            total += inputAmount[i];
+        }
+        return total;
+    }
+
+
+    public int totalOutputAmount() {
+        int total = 0;
+        for (int i=0; i < outputAmount.length; i++) {
+            total += outputAmount[i];
+        }
+        return total;
+    }
+
+}
