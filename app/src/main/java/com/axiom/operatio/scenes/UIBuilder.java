bdcd7eb6aa9e7b5171339112616eb0ee18f57082
Bolat Basheyev <b.basheyev@gmail.com>
2020-09-02 22:36:23 +0600

    Test user interface added

--- app/src/main/java/com/axiom/operatio/scenes/UIBuilder.java
+++ app/src/main/java/com/axiom/operatio/scenes/UIBuilder.java
@@ -2,9 +2,12 @@ package com.axiom.operatio.scenes;
 
 import android.content.res.Resources;
 import android.graphics.Color;
+import android.transition.Scene;
 import android.util.Log;
 
 import com.axiom.atom.R;
+import com.axiom.atom.engine.core.GameScene;
+import com.axiom.atom.engine.core.SceneManager;
 import com.axiom.atom.engine.graphics.renderers.Sprite;
 import com.axiom.atom.engine.ui.listeners.ClickListener;
 import com.axiom.atom.engine.ui.widgets.Button;
@@ -13,7 +16,7 @@ import com.axiom.atom.engine.ui.widgets.Widget;
 
 public class UIBuilder {
 
-    public static void buildUI(Resources resources, Widget widget) {
+    public static void buildUI(final Resources resources, Widget widget) {
 
         ClickListener listener = new ClickListener() {
             @Override
@@ -21,6 +24,14 @@ public class UIBuilder {
                 Log.i("INPUT", "BUTTON PRESSED " + w.toString());
             }
         };
+
+        ClickListener exitListener = new ClickListener() {
+            @Override
+            public void onClick(Widget w) {
+                SceneManager.getInstance(resources).exitGame();
+            }
+        };
+
 
         Widget panel = new Panel();
         panel.setLocalBounds(0,0,1920,180);
@@ -31,7 +42,7 @@ public class UIBuilder {
         Widget button = new Button("EXIT BUTTON");
         button.setLocalBounds(1680,40,200,100);
         button.setColor(Color.GREEN);
-        button.setClickListener(listener);
+        button.setClickListener(exitListener);
         panel.addChild(button);
 
         Widget button2 = new Button(new Sprite(resources, R.drawable.material));
