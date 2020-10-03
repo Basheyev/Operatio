06b94317b4c906b1e563ca501ffbdcd6adae98f9
Bolat Basheyev <b.basheyev@gmail.com>
2020-10-03 08:35:53 +0600

    Vertical flip fixed

--- app/src/main/java/com/axiom/atom/engine/graphics/gles2d/TextureAtlas.java
+++ app/src/main/java/com/axiom/atom/engine/graphics/gles2d/TextureAtlas.java
@@ -12,11 +12,23 @@ public class TextureAtlas {
 
     public static final int MIN_CAPACITY = 16;
 
-    protected Texture texture;
+    protected float textureWidth, textureHeight;
     protected ArrayList<TextureRegion> regions;
 
+    public TextureAtlas(float textureWidth, float textureHeight) {
+        this.textureWidth = textureWidth;
+        this.textureHeight = textureHeight;
+        regions = new ArrayList<TextureRegion>(MIN_CAPACITY);
+    }
+
+    public TextureAtlas(float textureWidth, float textureHeight, int columns, int rows) {
+        this(textureWidth, textureHeight);
+        generateTilemap(columns, rows);
+    }
+
     public TextureAtlas(Texture texture) {
-        this.texture = texture;
+        this.textureWidth = texture.width;
+        this.textureHeight = texture.height;
         regions = new ArrayList<TextureRegion>(MIN_CAPACITY);
     }
 
@@ -29,6 +41,7 @@ public class TextureAtlas {
         // Уходим если не валидные параметры
         if (name==null || (x < 0) || (y < 0) || (width<1) || (height<1)) return null;
         // Сохраняем информацию о регионе
+        // TODO Тут надо переварачивать координаты
         TextureRegion region = new TextureRegion();
         region.name = name;
         region.x = x;
@@ -37,10 +50,11 @@ public class TextureAtlas {
         region.height = height;
         region.textureCoordinates = new float[12];
         // Нормируем координаты на текстурные координаты - единицу (0.0-1.0)
-        float x1 = x / texture.width;
-        float y1 = y / texture.height;
-        float x2 = (x + width) / texture.width;
-        float y2 = (y + height) / texture.height;
+        // и переворачиваем (в Bitmap Y=0 сверху, а в текстуре Y=0 снизу)
+        float x1 = x / textureWidth;
+        float y1 = 1.0f - ((y + height) / textureHeight);
+        float x2 = (x + width) / textureWidth;
+        float y2 = 1.0f - (y / textureHeight);
         // ===== Треугольник 1
         region.textureCoordinates[0] = x1;  // левый верхний угол
         region.textureCoordinates[1] = y2;
@@ -69,6 +83,10 @@ public class TextureAtlas {
         return null;
     }
 
+    public ArrayList<TextureRegion> getRegions() {
+        return regions;
+    }
+
     public TextureRegion getRegion(int index) {
         return regions.get(index);
     }
@@ -91,12 +109,12 @@ public class TextureAtlas {
      * @param rows количество строк
      */
     protected void generateTilemap(int columns, int rows) {
-        int width = (int) texture.getWidth();
-        int height = (int) texture.getHeight();
+        int width = (int) textureWidth;
+        int height = (int) textureHeight;
         int spriteWidth = width / columns;
         int spriteHeight = height / rows;
         int i = 0;
-        for (int y=height-spriteHeight; y>=0; y-=spriteHeight) {
+        for (int y=0; y<height; y+=spriteHeight) {
             for (int x=0; x<width; x+=spriteWidth) {
                 addRegion("Frame " + i, x, y, spriteWidth, spriteHeight);
                 i++;
