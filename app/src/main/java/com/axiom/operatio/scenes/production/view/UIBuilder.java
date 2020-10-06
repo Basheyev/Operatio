b0b5fb3aa93e592dd973226110b2ee3b39b664dc
Bolat Basheyev <b.basheyev@gmail.com>
2020-10-06 22:00:48 +0600

    Buttons sounds added

--- app/src/main/java/com/axiom/operatio/scenes/production/view/UIBuilder.java
+++ app/src/main/java/com/axiom/operatio/scenes/production/view/UIBuilder.java
@@ -2,8 +2,10 @@ package com.axiom.operatio.scenes.production.view;
 
 import android.content.res.Resources;
 
+import com.axiom.atom.R;
 import com.axiom.atom.engine.core.SceneManager;
 import com.axiom.atom.engine.graphics.gles2d.Camera;
+import com.axiom.atom.engine.sound.SoundRenderer;
 import com.axiom.atom.engine.ui.listeners.ClickListener;
 import com.axiom.atom.engine.ui.widgets.Button;
 import com.axiom.atom.engine.ui.widgets.Widget;
@@ -15,6 +17,7 @@ public class UIBuilder {
     protected static Button pauseButton;
     protected static BlocksPanel blocksPanel;
     protected static ModePanel editorPanel;
+    protected static int tickSound;
 
     public static void setPausedButtonState(boolean paused) {
         if (paused) {
@@ -33,9 +36,12 @@ public class UIBuilder {
 
         production = prod;
 
+        tickSound = SoundRenderer.loadSound(R.raw.tick_snd);
+
         ClickListener pauseListener = new ClickListener() {
             @Override
             public void onClick(Widget w) {
+                SoundRenderer.playSound(tickSound);
                 if (production.isPaused()) {
                     production.setPaused(false);
                     setPausedButtonState(false);
@@ -49,6 +55,7 @@ public class UIBuilder {
         ClickListener exitListener = new ClickListener() {
             @Override
             public void onClick(Widget w) {
+                SoundRenderer.playSound(tickSound);
                 if (!production.isPaused()) {
                     production.setPaused(true);
                     UIBuilder.setPausedButtonState(true);
