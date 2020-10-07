7b186ba3cc076b8e418ae8d0beaef93dc29e3048
Bolat Basheyev <b.basheyev@gmail.com>
2020-10-07 09:32:50 +0600

    Refactored

--- app/src/main/java/com/axiom/operatio/MainActivity.java
+++ app/src/main/java/com/axiom/operatio/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.axiom.atom.engine.core.GameView;
+import com.axiom.atom.tests.shoottest.ShooterScene;
 import com.axiom.operatio.scenes.mainmenu.MainMenuScene;
 
 public class MainActivity extends AppCompatActivity {
