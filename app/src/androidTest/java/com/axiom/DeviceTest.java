3a685cf1679a60c1a09c7a32ba2c83a6991a85fc
Bolat Basheyev <b.basheyev@gmail.com>
2020-10-11 20:17:22 +0600

    Refactored

--- app/src/androidTest/java/com/axiom/DeviceTest.java
+++ app/src/androidTest/java/com/axiom/DeviceTest.java
@@ -1,12 +1,22 @@
 package com.axiom;
 
+import android.app.Instrumentation;
 import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.axiom.atom.R;
+import com.axiom.atom.engine.data.JSONAtlas;
+import com.axiom.atom.engine.graphics.gles2d.TextureAtlas;
+import com.axiom.atom.engine.graphics.gles2d.TextureRegion;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -16,7 +26,7 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class DeviceTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
