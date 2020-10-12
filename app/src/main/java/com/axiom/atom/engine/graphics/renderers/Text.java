17420ba839e0d73ac32db76a61c20fdd58aef404
Bolat Basheyev <b.basheyev@gmail.com>
2020-10-12 11:39:01 +0600

    Multiple instatnaces bouncing characters bug fixed

--- app/src/main/java/com/axiom/atom/engine/graphics/renderers/Text.java
+++ app/src/main/java/com/axiom/atom/engine/graphics/renderers/Text.java
@@ -9,7 +9,6 @@ import android.graphics.Rect;
 import android.graphics.Typeface;
 
 import com.axiom.atom.engine.core.geometry.AABB;
-import com.axiom.atom.engine.graphics.GraphicsRender;
 import com.axiom.atom.engine.graphics.gles2d.Camera;
 import com.axiom.atom.engine.graphics.gles2d.Texture;
 import com.axiom.atom.engine.graphics.gles2d.TextureAtlas;
@@ -24,34 +23,28 @@ import java.util.HashMap;
  */
 public class Text {
 
-    protected static HashMap<String, Sprite> generatedFonts = new HashMap<>();
+    protected static HashMap<String, RasterizedFont> fonts = new HashMap<>();
+    protected static int fontSize = 24;     // Размер генерируемого шрифта в пикселях
+
+    public static class RasterizedFont {
+        protected int totalChars = 121;     // Количество генерируемых символов (11x11)
+        protected Sprite fontSprite;        // Спрайт где каждый кадр это отдельный символ
+        protected float[] xOffset;          // Горизонтальное смещение символа относительно курсора
+        protected float[] yOffset;          // Вертикальное смещение символа относительно курсора
+        protected float maxLineHeight;      // Высота строки этого шрифта
+        protected float spacing;            // Расстояние между символами
+    }
 
     public int zOrder = 0;
-    protected Sprite fontSprite;           // Спрайт где каждый кадр это отдельный символ
-    protected int totalChars = 121;        // Количество генерируемых символов
-    protected int fontSize = 24;           // Размер генерируемого шрифта в пикселях
-    protected float[] xOffset;             // Горизонтальное смещение символа относительно курсора
-    protected float[] yOffset;             // Вертикальное смещение символа относительно курсора
-    protected float maxLineHeight;         // Высота строки этого шрифта
-    protected float spacing;               // Расстояние между символами
+    protected RasterizedFont font;
 
-    /**
-     * Закрытый конструктор чтобы создать массивы смещений
-     */
-    private Text() {
-        xOffset = new float[totalChars];
-        yOffset = new float[totalChars];
-    }
 
     /**
      * Конструктор чтобы сформировать шрифт по названию
      * @param fontName название шрифта
      */
     public Text(String fontName) {
-        this();
-        this.fontSprite = generateFontSprite(fontName, fontSize);
-        this.fontSprite.useColor = true;
-        this.spacing = 1;
+        this.font = rasterizeFont(fontName, fontSize);
     }
 
     /**
@@ -68,11 +61,14 @@ public class Text {
      * @param spacing множитель отступа
      */
     public Text(Sprite fontSprite, float spacing) {
-        this();
-        this.fontSprite = fontSprite;
-        this.spacing = spacing;
-        this.totalChars = fontSprite.getFramesAmount();
-        this.maxLineHeight = getTextHeight("|", 1);
+        RasterizedFont fnt = new RasterizedFont();
+        fnt.fontSprite = fontSprite;
+        fnt.spacing = spacing;
+        fnt.totalChars = fontSprite.getFramesAmount();
+        fnt.xOffset = new float[fnt.totalChars];
+        fnt.yOffset = new float[fnt.totalChars];
+        fnt.maxLineHeight = getTextHeight("|", 1);
+        this.font = fnt;
     }
 
     /**
@@ -98,7 +94,7 @@ public class Text {
      */
     public void draw(Camera camera, CharSequence text, float x, float y, float scale, AABB scissor) {
         // Указываем спрайту z-слой на котором рисуем
-        fontSprite.zOrder = zOrder;
+        font.fontSprite.zOrder = zOrder;
         // Начальные координаты
         float cursorX = x;
         float cursorY = y;
@@ -112,28 +108,30 @@ public class Text {
             symbolIndex = symbol - ' ';                       // Вычисляем его индекс (кадра)
 
             if (symbol=='\n') {                               // Если это перенос строки,
-                // Тут нужна правка по высоте
-                cursorY -= maxLineHeight * scale;             // Смещаемся по вертикали вниз
+                cursorY -= font.maxLineHeight * scale;        // Смещаемся по вертикали вниз
                 cursorX = x;                                  // Переходим на начало строки
                 lastSymbol = symbol;
                 continue;                                     // Переходим к следующему символу
             }
 
             // Если по какой-то причине индекс больше количества символов, идём дальше
-            if (symbolIndex < 0 || symbolIndex >= totalChars) continue;
+            if (symbolIndex < 0 || symbolIndex >= font.totalChars) continue;
 
             // Если это не первый символ в строке добавляем смещение курсора предыдущего символа
             if (i!=0 && lastSymbol!='\n') {
-                cursorX += fontSprite.getWidth() * scale * spacing;
+                cursorX += font.fontSprite.getWidth() * scale * font.spacing;
             }
 
-            fontSprite.setActiveFrame(symbolIndex);
+            // Переходим на кадр нужного нам символа
+            font.fontSprite.setActiveFrame(symbolIndex);
 
-            float yPos = cursorY + (yOffset[symbolIndex] * scale);
+            // Считаем смещение символа относительно курсора по высоте
+            float yPos = cursorY + (font.yOffset[symbolIndex] * scale);
 
-            fontSprite.draw(camera, cursorX, yPos,
-                    fontSprite.getWidth() * scale,
-                    fontSprite.getHeight() * scale, scissor);
+            // Отрисовываем символ
+            font.fontSprite.draw(camera, cursorX, yPos,
+                    font.fontSprite.getWidth() * scale,
+                    font.fontSprite.getHeight() * scale, scissor);
 
             lastSymbol = symbol;
         }
@@ -148,7 +146,7 @@ public class Text {
      */
     public float getTextWidth(CharSequence text, float scale) {
         // Получаем все кадры (символы) спрайта
-        ArrayList<TextureRegion> regions = fontSprite.getAtlas().getRegions();
+        ArrayList<TextureRegion> regions = font.fontSprite.getAtlas().getRegions();
         char symbol;
         int symbolIndex;
         int symbolWidth;
@@ -163,7 +161,7 @@ public class Text {
             }
             symbolIndex = symbol - ' ';
             symbolWidth = (symbolIndex >= 0 && symbolIndex < regions.size()) ? regions.get(symbolIndex).width : 0;
-            totalLineWidth += symbolWidth * scale * spacing;
+            totalLineWidth += symbolWidth * scale * font.spacing;
         }
         if (totalLineWidth > maxLineWidth) maxLineWidth = totalLineWidth;
         return maxLineWidth;
@@ -171,7 +169,7 @@ public class Text {
 
     public float getTextHeight(CharSequence text, float scale) {
         // Получаем все кадры (символы) спрайта
-        ArrayList<TextureRegion> regions = fontSprite.getAtlas().getRegions();
+        ArrayList<TextureRegion> regions = font.fontSprite.getAtlas().getRegions();
         int symbolIndex;
         int symbolHeight;
         int maxHeight = 0;
@@ -192,7 +190,7 @@ public class Text {
     }
 
     public void setColor(float r, float g, float b, float a) {
-        fontSprite.setColor(r, g, b, a);
+        font.fontSprite.setColor(r, g, b, a);
     }
 
     /**
@@ -201,14 +199,20 @@ public class Text {
      * @param size размер шрифта в пикселях
      * @return спрайт где каждому региону соответствует символ
      */
-    protected Sprite generateFontSprite(String fontName, int size) {
+    protected RasterizedFont rasterizeFont(String fontName, int size) {
 
-        Sprite generatedFont = generatedFonts.get(fontName);
-        if (generatedFont!=null) return generatedFont;
+        // Если уже растеризовали этот шрифт то просто его возвращаем
+        RasterizedFont rasterizedFont = fonts.get(fontName);
+        if (rasterizedFont!=null) return rasterizedFont;
+
+        // Если такого не делали, начинаем создавать растеризованный шрифт
+        rasterizedFont = new RasterizedFont();
+        rasterizedFont.xOffset = new float[rasterizedFont.totalChars];
+        rasterizedFont.yOffset = new float[rasterizedFont.totalChars];
 
         // Чтобы генерируемая текстура была квадратная
         // считаем сколько символов по вертикали/горизонтали будет
-        int matrixSize = (int) Math.ceil(Math.sqrt(totalChars));
+        int matrixSize = (int) Math.ceil(Math.sqrt(rasterizedFont.totalChars));
 
         // Создаем соответствующего размера Bitmap с прозрачностью
         Bitmap bitmap = Bitmap.createBitmap(
@@ -241,7 +245,7 @@ public class Text {
         int symbolIndex;
         Rect symbolRectangle = new Rect();
 
-        maxLineHeight = size;
+        rasterizedFont.maxLineHeight = size;
 
         for (int y=1; y<matrixSize-1; y++) {
             for (int x=0; x<matrixSize; x++) {
@@ -263,7 +267,7 @@ public class Text {
                 int y2 = 1 + y * size + symbolRectangle.bottom;
 
                 // Dычисляем смещение символа по вертикали
-                yOffset[symbolIndex] = -symbolRectangle.bottom;
+                rasterizedFont.yOffset[symbolIndex] = -symbolRectangle.bottom;
 
                 // Считаем высоту и ширину символа
                 int width = x2 - x1;
@@ -281,10 +285,16 @@ public class Text {
             }
         }
 
-        generatedFont = new Sprite(Texture.getInstance(bitmap, true), textureAtlas);
-        generatedFonts.put(fontName, generatedFont);
+        // Дополняем информацию растеризованного шрифта
+        rasterizedFont.fontSprite = new Sprite(Texture.getInstance(bitmap, true), textureAtlas);
+        rasterizedFont.fontSprite.useColor = true;  // Флаг для шейдера закрашивающего цветом
+        rasterizedFont.spacing = 1;
 
-        return generatedFont;
+        // Добавляем в общий список растеризованых шрифтов
+        fonts.put(fontName, rasterizedFont);
+
+        // Возвращаем растеризованый шрифт
+        return rasterizedFont;
     }
 
 
