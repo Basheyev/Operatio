a48619e8fb35cb01bf33035c3a06a6a847f3f336
Bolat Basheyev <b.basheyev@gmail.com>
2020-10-17 13:41:07 +0600

    Warehouse scene added

--- app/src/main/java/com/axiom/operatio/scenes/warehouse/WarehouseScene.java
+++ app/src/main/java/com/axiom/operatio/scenes/warehouse/WarehouseScene.java
@@ -2,20 +2,41 @@ package com.axiom.operatio.scenes.warehouse;
 
 import android.view.MotionEvent;
 
+import com.axiom.atom.R;
 import com.axiom.atom.engine.core.GameScene;
+import com.axiom.atom.engine.core.SceneManager;
 import com.axiom.atom.engine.graphics.gles2d.Camera;
+import com.axiom.atom.engine.graphics.renderers.Sprite;
+import com.axiom.atom.engine.sound.SoundRenderer;
+import com.axiom.atom.engine.ui.listeners.ClickListener;
+import com.axiom.atom.engine.ui.widgets.Button;
+import com.axiom.atom.engine.ui.widgets.Panel;
+import com.axiom.atom.engine.ui.widgets.Widget;
+import com.axiom.operatio.model.materials.Material;
+import com.axiom.operatio.model.production.Production;
+import com.axiom.operatio.model.warehouse.Warehouse;
+import com.axiom.operatio.scenes.production.view.ItemWidget;
+import com.axiom.operatio.scenes.production.view.ProductionSceneUI;
+
+import static android.graphics.Color.DKGRAY;
 
 
 // TODO 1. Добавить сцену склад: хранение материалов и машин
 // TODO 2. Добавить сцену склад: правила покупки и продажи со склада (симуляция рынка цен)
 public class WarehouseScene extends GameScene {
+
+    protected static boolean initialized = false;
+    protected static int tickSound;
+    protected static Sprite background;
+
     @Override
     public String getSceneName() {
-        return null;
+        return "Warehouse";
     }
 
     @Override
     public void startScene() {
+        if (!initialized) buildUI();
 
     }
 
@@ -31,7 +52,8 @@ public class WarehouseScene extends GameScene {
 
     @Override
     public void preRender(Camera camera) {
-
+        background.zOrder = 0;
+        background.draw(camera,camera.getMinX(),camera.getMinY(), Camera.WIDTH,Camera.HEIGHT);
     }
 
     @Override
@@ -41,6 +63,55 @@ public class WarehouseScene extends GameScene {
 
     @Override
     public void onMotion(MotionEvent event, float worldX, float worldY) {
+
+    }
+
+
+    protected void buildUI() {
+        background = new Sprite(SceneManager.getResources(), R.drawable.background);
+        tickSound = SoundRenderer.loadSound(R.raw.tick_snd);
+
+        ClickListener exitListener = new ClickListener() {
+            @Override
+            public void onClick(Widget w) {
+                SoundRenderer.playSound(tickSound);
+                SceneManager.getInstance().setActiveScene("Production");
+            }
+        };
+
+        Widget widget = getSceneWidget();
+        Button exitButton = new Button("Production");
+        exitButton.setTextColor(1,1,1,1);
+        exitButton.setLocalBounds(Camera.WIDTH - 375,960,375,100);
+        exitButton.setColor(0.8f, 0.5f, 0.5f, 0.9f);
+        exitButton.setClickListener(exitListener);
+        widget.addChild(exitButton);
+
+
+        Panel panel = new Panel();
+        panel.setLocalBounds(50,50, Camera.WIDTH - 600, Camera.HEIGHT - 200);
+        panel.setColor(0xCC505050);
+
+        Warehouse warehouse = Warehouse.getInstance();
+        float x = 30, y = 700;
+        for (int i=0; i< Material.getMaterialsAmount(); i++) {
+            Material material = Material.getMaterial(i);
+            int balance = warehouse.getBalance(material);
+            ItemWidget itemWidget = new ItemWidget("" + balance);
+            itemWidget.setColor(0x10101050);
+            itemWidget.setBackground(material.getImage());
+            itemWidget.setTextScale(1);
+            itemWidget.setLocalBounds(x, y, 80, 80);
+            panel.addChild(itemWidget);
+            x += 96;
+            if (x + 96 > panel.getWidth()) {
+                x = 30;
+                y -= 96;
+            }
+        }
+        widget.addChild(panel);
+
+        initialized = true;
 
     }
 }
