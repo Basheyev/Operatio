6e48407dd242179ce29bb632ded1646a49e2497c
Bolat Basheyev <b.basheyev@gmail.com>
2021-04-06 09:20:55 +0600

    slide numbers added

--- app/src/main/java/com/axiom/operatio/scenes/mainmenu/StoryPanel.java
+++ app/src/main/java/com/axiom/operatio/scenes/mainmenu/StoryPanel.java
@@ -1,5 +1,7 @@
 package com.axiom.operatio.scenes.mainmenu;
 
+import android.graphics.Color;
+
 import com.axiom.atom.R;
 import com.axiom.atom.engine.core.SceneManager;
 import com.axiom.atom.engine.core.geometry.AABB;
@@ -8,6 +10,7 @@ import com.axiom.atom.engine.graphics.gles2d.Camera;
 import com.axiom.atom.engine.graphics.gles2d.Texture;
 import com.axiom.atom.engine.graphics.renderers.Sprite;
 import com.axiom.atom.engine.ui.widgets.Panel;
+import com.axiom.operatio.model.common.FormatUtils;
 
 import static android.graphics.Color.BLACK;
 
@@ -19,12 +22,16 @@ public class StoryPanel extends Panel {
     public static final int PANEL_COLOR = 0xCC505050;
 
     private Sprite storyFrames, ceo;
+    private StringBuffer slideNumber = new StringBuffer();
+    private int animationID;
+    private int lastSlide = -1;
 
     public StoryPanel() {
         super();
         Texture storyTexture = Texture.getInstance(SceneManager.getResources(), R.drawable.story, true);
         storyFrames = new Sprite(storyTexture, 4,4);
-        storyFrames.setActiveAnimation(storyFrames.addAnimation(0,11, 0.2f, true));
+        animationID = storyFrames.addAnimation(0,11, 0.2f, true);
+        storyFrames.setActiveAnimation(animationID);
         Texture ceoTexture = Texture.getInstance(SceneManager.getResources(), R.drawable.ceo, true);
         ceo = new Sprite(ceoTexture, 1,1);
         opaque = false;
@@ -40,6 +47,20 @@ public class StoryPanel extends Panel {
         GraphicsRender.drawRectangle(bounds, null);
         storyFrames.setZOrder(zOrder + 1);
         storyFrames.drawExact(camera, bounds.minX + 15, bounds.minY + 15, bounds.maxX - 15, bounds.maxY - 15);
+        // Если слайд изменился
+        int activeFrame = storyFrames.getActiveFrame();
+        int totalFrames = storyFrames.getAnimation(animationID).stopFrame;
+        if (lastSlide != activeFrame) {
+            slideNumber.setLength(0);
+            slideNumber.append(activeFrame + 1);
+            slideNumber.append('/');
+            slideNumber.append(totalFrames + 1);
+            lastSlide = activeFrame;
+        }
+        GraphicsRender.setZOrder(zOrder + 2);
+        GraphicsRender.setColor(Color.BLACK);
+        GraphicsRender.drawText(slideNumber, bounds.centerX - 20, bounds.minY + 25, 1.5f);
+
         ceo.setZOrder(zOrder + 100);
         ceo.draw(camera, bounds.maxX - ceoW / 2, bounds.minY - ceoH / 2, ceoW, ceoH);
         super.draw(camera);
