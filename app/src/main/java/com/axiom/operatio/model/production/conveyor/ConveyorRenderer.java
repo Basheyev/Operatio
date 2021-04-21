19a439aada36bcf81cde3218516b599b1eb3cd24
Bolat Basheyev <b.basheyev@gmail.com>
2021-04-21 12:55:07 +0600

    Refactored

--- app/src/main/java/com/axiom/operatio/model/production/conveyor/ConveyorRenderer.java
+++ app/src/main/java/com/axiom/operatio/model/production/conveyor/ConveyorRenderer.java
@@ -29,14 +29,14 @@ public class ConveyorRenderer extends BlockRenderer {
     protected static Sprite allConveyors = null;
     protected Block block;                                       // Блок к которому привзян рендер
     protected Sprite sprite;                                     // Спрайт основного конвейера
-    protected Sprite leftJointSprite, rightJointSprite;            // Спрайты боковых соединений
+    protected Sprite leftJointSprite, rightJointSprite;          // Спрайты боковых соединений
     protected Sprite fault;                                      // Значек сбоя
+    protected ItemsRenderer itemsRenderer;
     protected int animStraight, animUpToRight, animRightToUp;
     protected int animL_U2R, animL_R2U;
     protected int animR_U2R, animR_R2U;
     protected Block lastLeftBlock, lastRightBlock;
 
-    private Vector coordBuffer = new Vector();
 
     public ConveyorRenderer(Block block) {
         this.block = block;
@@ -58,6 +58,8 @@ public class ConveyorRenderer extends BlockRenderer {
 
         createAnimations();
         adjustAnimation(block.getInputDirection(), block.getOutputDirection());
+
+        itemsRenderer = new ItemsRenderer(block, sprite);
     }
 
 
@@ -183,7 +185,7 @@ public class ConveyorRenderer extends BlockRenderer {
         sprite.draw(camera,x,y, width, height);
 
         // Отрисовать материалы
-        drawItems(camera, x, y, width, height);
+        itemsRenderer.drawItems(camera, x, y, width, height);
 
         // Отрисовать вход/выход если игра на паузе
         if (gamePaused) {
@@ -235,163 +237,6 @@ public class ConveyorRenderer extends BlockRenderer {
 
 
 
-    /**
-     * Рассчитывает прогресс движения предметов и отрисовывает предметы на конвейре
-     * @param camera камера
-     * @param x левая координата клетки
-     * @param y нижняя координата клетки
-     * @param width ширина клетки
-     * @param height высота клетки
-     */
-    protected void drawItems(Camera camera, float x, float y, float width, float height) {
-
-        Channel<Item> inputQueue = block.getInputQueue();                      // Входящая очередь
-        Channel<Item> outputQueue = block.getOutputQueue();                    // Исходящая очередь
-        float cycleTime = this.block.getProduction().getCycleMilliseconds();   // Длительность цикла в мс.
-
-        float deliveryCycles = 1;
-        if (block instanceof Conveyor) {
-            deliveryCycles = ((Conveyor) block).getDeliveryCycles();
-        } else if (block instanceof Machine) {
-            deliveryCycles = ((Machine) block).getOperation().getCycles() + 1;
-        }
-
-        float deliveryTime = deliveryCycles * cycleTime;                 // Время доставки в мс.
-        float capacity = block.getTotalCapacity();                       // Вместимость конвейера в предметах
-        float stridePerItem = 1.0f / (capacity / 2.0f);                  // Шаг для одного предмета
-        float progress;
-
-        long now = block.getProduction().getClock();
-        float cycleBias = (now - block.getLastPollTime()) / cycleTime;
-        if (cycleBias > 1.0f) cycleBias = 1.0f;
-
-        float progressBias = cycleBias * stridePerItem;
-        int finishedCounter = block.getOutputQueue().size();
-
-        for (int i=0; i<outputQueue.size(); i++) {
-            Item item = outputQueue.get(i);
-            if (item==null) continue;
-            progress = 1.0f - (i * stridePerItem) + progressBias - stridePerItem;
-            if (block instanceof Machine) {
-                if (progress < 0.5f) progress = 0.5f;
-            }
-            drawItem (camera, x, y, width, height, item, progress);
-        }
-
-
-        float maxProgress = 1.0f - (finishedCounter * stridePerItem);
-        for (int i=0; i<inputQueue.size(); i++) {
-            Item item = inputQueue.get(i);
-            if (item == null) continue;
-            // fixme BUG: если конвейер остановлен Bias время item продолжает идти (считать время остановки)
-            now = block.getProduction().getClock();
-            progress = (now - item.getTimeOwned()) /  deliveryTime;
-            if (progress > maxProgress) {
-                progress = maxProgress;
-                maxProgress -= stridePerItem;
-            }
-            if (block instanceof Machine) {
-              if (progress > 0.5f) progress = 0.5f;
-              if (progress < 0.0f) progress = 0;
-            }
-            drawItem (camera, x, y, width, height, item, progress);
-        }
-
-    }
-
-
-    /**
-     * Отрисовывает предмет на основе координат клетки и прогресса движения
-     * @param camera камера
-     * @param x левая координата
-     * @param y нижняя координата
-     * @param width ширина клетки
-     * @param height высота клетки
-     * @param item предмет
-     * @param progress прогресс движения 0.0-1.0
-     */
-    protected void drawItem(Camera camera, float x, float y, float width, float height, Item item, float progress) {
-
-        int inputDirection = block.getInputDirection();
-        int outputDirection = block.getOutputDirection();
-
-        calculateCoordinates(progress, inputDirection, outputDirection, coordBuffer);
-        float xpos = coordBuffer.x;
-        float ypos = coordBuffer.y;
-        float minx = x + xpos * width + width / 4;
-        float miny = y + ypos * height + height / 4;
-
-        Sprite materialSprite = item.getMaterial().getImage();
-        materialSprite.setZOrder(sprite.getZOrder() + 1);
-        materialSprite.draw(camera,minx,miny,width / 2, height / 2);
-
-    }
-
-
-    /**
-     * Рассчитывает координаты с учетом направления движения и прогресса
-     * @param progress прогресс движения материала по конвейеру 0.0-1.0
-     * @param inpDir направление входа
-     * @param outDir направление выхода
-     * @param result расчитанные координаты
-     */
-    private void calculateCoordinates(float progress, int inpDir, int outDir, Vector result) {
-        float rads, xpos = 0, ypos = 0;
-
-        if (inpDir==LEFT && outDir==RIGHT) {
-            xpos = progress - 0.5f;
-            ypos = 0;
-        } else if (inpDir==RIGHT && outDir==LEFT) {
-            xpos = 1 - progress - 0.5f;
-            ypos = 0;
-        } else if (inpDir==DOWN && outDir== UP) {
-            xpos = 0;
-            ypos = progress - 0.5f;
-        } else if (inpDir== UP && outDir==DOWN) {
-            xpos = 0;
-            ypos = 1 - progress - 0.5f;
-        }
-        //-----------------------------------------------------------------------
-        else if (inpDir== RIGHT && outDir==UP) {
-            rads = (float) ((Math.PI*1.5) - (Math.PI/2 * progress));
-            xpos = (float) (Math.cos(rads) + 1) / 2;
-            ypos = (float) (Math.sin(rads) + 1) / 2;
-        } else if (inpDir== DOWN && outDir==RIGHT) {
-            rads = (float) ((Math.PI) - (Math.PI/2 * progress));
-            xpos = (float) (Math.cos(rads) + 1) / 2;
-            ypos = (float) (Math.sin(rads) - 1) / 2;
-        } else if (inpDir== LEFT && outDir==DOWN) {
-            rads = (float) ((Math.PI/2) - (Math.PI/2 * progress));
-            xpos = (float) (Math.cos(rads) - 1) / 2;
-            ypos = (float) (Math.sin(rads) - 1) / 2;
-        } else if (inpDir== UP && outDir==LEFT) {
-            rads = (float) -(Math.PI/2 * progress);
-            xpos = (float) (Math.cos(rads) - 1) / 2;
-            ypos = (float) (Math.sin(rads) + 1) / 2;
-        }
-        //-------------------------------------------------------------------------
-        else if (inpDir== UP && outDir==RIGHT) {
-            rads = (float) (Math.PI + (Math.PI / 2 * progress));
-            xpos = (float) (Math.cos(rads) + 1) / 2;
-            ypos = (float) (Math.sin(rads) + 1) / 2;
-        } else if (inpDir== LEFT && outDir==UP) {
-            rads = (float) ((Math.PI * 1.5) + (Math.PI / 2 * progress));
-            xpos = (float) (Math.cos(rads) - 1) / 2;
-            ypos = (float) (Math.sin(rads) + 1) / 2;
-        } else if (inpDir== DOWN && outDir==LEFT) {
-            rads = (float) (Math.PI / 2 * progress);
-            xpos = (float) (Math.cos(rads) - 1) / 2;
-            ypos = (float) (Math.sin(rads) - 1) / 2;
-        } else if (inpDir== RIGHT && outDir==DOWN) {
-            rads = (float) (Math.PI/2 + (Math.PI/2 * progress));
-            xpos = (float) (Math.cos(rads) + 1) / 2;
-            ypos = (float) (Math.sin(rads) - 1) / 2;
-        }
-
-        result.x = xpos;
-        result.y = ypos;
-
-    }
 
     @Override
     public void setAnimationSpeed(float speed) {
