bd5d7974420a6c8f57bd5f406662a30179a218a2
Bolat Basheyev <b.basheyev@gmail.com>
2022-03-08 12:06:31 +0600

    Refactored

--- app/src/main/java/com/axiom/operatio/model/market/CommodityMarket.java
+++ app/src/main/java/com/axiom/operatio/model/market/CommodityMarket.java
@@ -2,13 +2,16 @@ package com.axiom.operatio.model.market;
 
 import com.axiom.operatio.model.ledger.Ledger;
 
+/**
+ * Симулятор рынка товара - выдает значение спроса, предложения и цены
+ */
 public class CommodityMarket {
 
     public static final int HISTORY_LENGTH = Ledger.HISTORY_LENGTH * 3;
 
-    public static final double LONG_CYCLE_PART  = 0.05;    // +-5% длинный экономический цикл
-    public static final double SHORT_CYCLE_PART = 0.03;    // +-2% короткий экономический цикл
-    public static final double NOISE_PART       = 0.10;    // +-10% рыночный шум
+    public static final double LONG_CYCLE_PART  = 0.05;    // +-5% влияние длинный экономический цикл
+    public static final double SHORT_CYCLE_PART = 0.03;    // +-3% влияние короткий экономический цикл
+    public static final double NOISE_PART       = 0.10;    // +-10% влияние рыночного шума
     public static final double SHORT_STEP       = 0.02;    // шаг на рынке по короткому циклу
     public static final double LONG_STEP        = 0.0005;  // шаг на рынке по длинному циклу
 
@@ -28,6 +31,12 @@ public class CommodityMarket {
     private int historyLength;
 
 
+    /**
+     * Конструктор симулятора рынка товара
+     * @param faceValue номинальная цена товара
+     * @param averageDemand средний спрос в количестве штук
+     * @param averageSupply среднее предложение в количчестве штук
+     */
     public CommodityMarket(double faceValue, double averageDemand, double averageSupply) {
         this.faceValue = faceValue;
         this.averageDemand = averageDemand;
@@ -42,12 +51,18 @@ public class CommodityMarket {
     }
 
 
+    /**
+     * Выполняет один шаг симуляции
+     */
     public synchronized void process() {
         evaluateNextValue();
         saveHistory();
     }
 
 
+    /**
+     * Считает следующие значение спроса, предложения и рыночной цены
+     */
     private void evaluateNextValue() {
         // Посчитать спрос
         double nextDemand = (averageDemand + demand) / 2.0;
@@ -66,10 +81,12 @@ public class CommodityMarket {
         // Сделать шаг по рыночному циклу
         marketCycle += SHORT_STEP * Math.random();
         largeCycle += LONG_STEP * Math.random();
-
     }
 
 
+    /**
+     * Сохраняет в историю значение цены товара (для графиков)
+     */
     private void saveHistory() {
         history[historyLength] = marketValue;
         historyLength++;
@@ -83,30 +100,63 @@ public class CommodityMarket {
         }
     }
 
+
+    /**
+     * Получить номинальную стоимость товара
+     * @return номинальная стоимость товара
+     */
     public double getFaceValue() {
         return faceValue;
     }
 
+
+    /**
+     * Получить рыночную стоимость товара
+     * @return рыночная стоимость товара
+     */
     public double getMarketValue() {
         return marketValue;
     }
 
+
+    /**
+     * Объем спроса товара на рынке
+     * @return спрос в количестве штук
+     */
     public double getDemand() {
         return demand;
     }
 
+
+    /**
+     * Объем предложения товара на рынке
+     * @return предложение в количестве штук
+     */
     public double getSupply() {
         return supply;
     }
 
+
+    /**
+     * Максимальная рыночная цена в истории
+     * @return максимальная рыночная цена в истории
+     */
     public double getHistoryMaxValue() {
         return historyMaxValue;
     }
 
+    /**
+     * Длина истории
+     * @return количество значений сохраненных в истории
+     */
     public int getHistoryLength() {
         return historyLength;
     }
 
+    /**
+     * Загрузить в массив данные истории рыночной цены
+     * @param destination массив со значениями истории рыночной цены
+     */
     public synchronized void getHistoryValues(double[] destination) {
         System.arraycopy(history, 0, destination, 0, historyLength);
     }
